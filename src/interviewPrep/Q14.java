99f6458825b0d960bfd70684ea3b0e5d4cc03170
yeni <halukbilgin.uk@gmail.com>
2021-12-20 15:35:50 +0300

    20/12

--- src/interviewPrep/Q14.java
+++ src/interviewPrep/Q14.java
@@ -3,6 +3,14 @@ package interviewPrep;
 public class Q14 {
     public static void main(String[] args) {
         /*
+      If the array is { 3, 5, 1, 2, 7, 9, 2, 3, 5, 7 }
+      type a program to create a new array whose elements are like { 3, 5+1, 2+7+9, 2+3+5+7 }
+
+      Verilen bir arayın elemanlarının ardışık artan toplamını hesaplayıp yeni bir array'a atayan bir program cretae ediniz..
+
+      input : { 3, 5, 1, 2, 7, 9, 2, 3, 5, 7 }
+
+      output : { 3, 5+1, 2+7+9, 2+3+5+7 }
 
          */
     }
